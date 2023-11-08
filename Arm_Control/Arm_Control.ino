b28e37d0981d2c497a6326770ccc9d35570fbef3
cmot17 <25753364+cmot17@users.noreply.github.com>
2023-11-08 14:05:36 -0500

    add more debug and change serial handling slightly

--- Arm_Control/Arm_Control.ino
+++ Arm_Control/Arm_Control.ino
@@ -17,17 +17,38 @@ float potVal;
 void setup() {
   stepper.connectToPins(MOTOR_STEP_PIN, MOTOR_DIRECTION_PIN);
   pinMode(potPin, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  stepper.setStepsPerRevolution(1600);
-  stepper.setSpeedInRevolutionsPerSecond(20);
-  stepper.setAccelerationInRevolutionsPerSecondPerSecond(20);
+  stepper.setStepsPerRevolution(1600 * 3);
+  stepper.setSpeedInRevolutionsPerSecond(1);
+  stepper.setAccelerationInRevolutionsPerSecondPerSecond(1);
 
-  if (Serial.available() > 0) {
-    x_coord = Serial.parseFloat();
-    theta = acos(x_coord / arm_length);
-    stepper.moveToPositionInRevolutions(theta / (2 * PI));
+  while (Serial.available() > 0) {
+    // read the first byte in the serial buffer
+    char inputChar = Serial.peek();
+    
+    if (inputChar == 'c') {
+      // If it's 'c', set the stepper to zero and consume the character
+      Serial.read(); // consume the 'c' character
+      Serial.println("Setting stepper position to zero.");
+      stepper.setCurrentPositionInRevolutions(0);
+    } else {
+      // Otherwise, parse the float number and perform the calculations
+      float x_coord = Serial.parseFloat();
+      Serial.println("Received x coordinate: ");
+      Serial.println(x_coord);
+      float theta = acos(x_coord / arm_length);
+      Serial.println("Moving to position in revolutions: ");
+      Serial.println(theta / (2 * PI));
+      stepper.setTargetPositionInRevolutions(theta / (2 * PI));
+    }
+  }
+
+  while(!stepper.motionComplete())
+  {
+    stepper.processMovement();       // this call moves the motor
   }
   // x_coord = Serial.parseFloat();
   // Serial.print("X coordinate: ");
