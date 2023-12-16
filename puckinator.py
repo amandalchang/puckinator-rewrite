f40edc72cd958284c95ec5c4de49ff32dbfee681
Amanda Chang <amandachang@Amandas-MacBook-Air-2.local>
2023-12-15 21:11:19 -0500

    Added docstrings

--- puckinator.py
+++ puckinator.py
@@ -34,7 +34,8 @@ PERF_COUNTER_ENABLED = False  # disable perf_counter for less print statements
 @dataclass
 class TimestampedPos:
     """
-    This is in terms of OpenCV coordinates in pixels and timestamp is in seconds
+    This organizes position and time information for the puck. 
+    x and y are defined in OpenCV pixel coordinates and timestamp is in seconds
     """
 
     x: float  # pixels
@@ -48,7 +49,11 @@ class TimestampedPos:
 @dataclass
 class TrajectoryPrediction:
     """
-    This is in terms of OpenCV coordinates in pixels and speed is in inches/second
+    This organizes the predicted point and the predicted time it will reach it.
+    It also includes the prediction vector.
+
+    x_int and y_int are in OpenCV pixel coordinates while vx and vy are in 
+    inches/sec
     """
 
     x_int: float  # Inches
@@ -59,6 +64,16 @@ class TrajectoryPrediction:
 
 
 def send_coordinates_to_arduino(desired_y, arduino, desired_x=HITTING_POSITION):
+    """
+    Sends the angles necessary to move the striker to a desired coordinate over 
+    serial to the arduino
+
+    Args: 
+        desired_y: a float representing the desired y coordinate of the striker
+        arduino: an instance of the serial.Serial class
+    Returns:
+        None
+    """
     (theta, phi) = coordinateconverter(
         desired_x + X_OFFSET,
         round((desired_y / PIXELS_PER_INCH) - Y_OFFSET, 2),
@@ -99,9 +114,10 @@ def predict_trajectory(
 
     Returns:
         TrajectoryPrediction: An instance of TrajectoryPrediction containing
-        the predicted x and y values of the puck, the dx and dy values, and
-        the intercept time. If the dy, dx, or intercept_time is None, the
-        function returns None.
+            the predicted x and y values of the puck, the dx and dy values, and
+            the intercept time. If the dy, dx, or intercept_time is None, the
+            function returns None.
+        puck_points: A list containing tuples of each predicted bounce point
     """
     time_elapsed = latest_pos.timestamp - prev_pos.timestamp
     dy = None
@@ -178,6 +194,22 @@ def predict_trajectory(
 
 
 def predict_next_bounce(m, b):
+    """
+    Predicts the next bounce point and velocity line based off the current 
+    velocity line using the law of reflection; this is only called when the
+    puck is heading towards the robot, eliminating the need for directionality
+    on the velocity line
+
+    Args:
+        m: a float representing the slope of a velocity line
+        b: a float representing the y-intercept of a velocity line
+
+    Returns:
+        (bounce_m, c): a tuple of floats containing the predicted slope and
+            y-intercept of after a bounce
+        (x1, y1): a tuple of ints containing the pixel coordinate of the
+            predicted bounce.
+    """
     if m == 0:
         m = 0.0001
     if m > 0:
@@ -193,21 +225,23 @@ def predict_next_bounce(m, b):
 
 def coordinateconverter(cY, cX, arm_length, displacement):
     """
+    This function returns the desired angles of the two
+    motors on a five-bar parallel robot relative to the horizontal given
+    the length of the arms (assumed to be of equal length) and the distance
+    between the motors. They must be in the same length units.
+
     Note:
         The origin is defined to be at the axis of movement of the left motor.
-        The x direction here is the y direction outside of this function
-        This function is designed to return the desired angles of the two
-        motors on a five-bar parallel robot relative to the horizontal given
-        the length of the arms (assumed to be of equal length) and the distance
-        between the motors. They must be in the same length units.
+        The x direction here is the y direction outside of this function.
     Args:
-        cX: The x coordinate of the center of the striker based on arm coordinates
-        cY: The y coordinate of the center of the striker
-        arm_length: The length of each of the four arms (inches)
-        displacement: The distance between the motors (inches)
+        cX: a float representing the x coordinate of the center of the striker 
+            based on arm coordinates
+        cY: a float representing he y coordinate of the center of the striker
+        arm_length: The float length of each of the four arms (inches)
+        displacement: The float distance between the motors (inches)
     Returns:
-        q1: the radian CCW angle of the left motor from the horizontal
-        q2: the radian CCW angle of the right motor from the horizontal
+        theta: the float radian CCW angle of the left motor from the horizontal
+        phi: the float radian CCW angle of the right motor from the horizontal
     """
     # Length of the diagonal between the origin and (cX, cY)
     diag1 = np.sqrt(cX**2 + cY**2)
@@ -228,6 +262,23 @@ def coordinateconverter(cY, cX, arm_length, displacement):
 
 class PerspectiveCorrector:
     def __init__(self, width, height) -> None:
+        """
+        Initialize a PerspectiveCorrector instance.
+
+        Args:
+            width (int): Width of the corrected frame.
+            height (int): Height of the corrected frame.
+
+        Attributes:
+        - dictionary: Predefined dictionary for ArUco marker detection.
+        - parameters: Detector parameters for configuring ArUco detection.
+        - detector: ArucoDetector object with the predefined dictionary and 
+            custom parameters.
+        - calibrated_transform (numpy.ndarray): Perspective transformation 
+            matrix after calibration.
+        - width (int): Width of the corrected frame.
+        - height (int): Height of the corrected frame.
+        """
         # Load a predefined dictionary for ArUco marker detection
         self.dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
         # Create an instance of DetectorParameters for configuring ArUco detection
@@ -240,6 +291,17 @@ class PerspectiveCorrector:
         self.height = height
 
     def calibrate(self, frame):
+        """
+        Computes the necessary transform to crop and distort the frame based on
+        the positions of the four ArUco markers. Stores the calibration 
+        distortion matrix in self.calibrated_transform.
+
+        Args:
+            frame (numpy.ndarray): Input image frame.
+
+        Returns:
+            None
+        """
         # Detect ArUco markers in the frame
         markerCorners, markerIds, _ = self.detector.detectMarkers(frame)
 
@@ -255,15 +317,9 @@ class PerspectiveCorrector:
                     zip(*sorted(detectedMarkers, key=lambda marker: marker[1]))
                 )[0]
 
-                # print(f"Sorted markers:\n{sorted_markers}")
-
                 desired_corners = np.array(
                     [marker[0][0] for marker in sorted_markers]
                 )  # Extracting the first corner of each marker
-
-                # print(
-                #     f"Desired corners (has shape {desired_corners.shape}):\n{desired_corners}"
-                # )
                 # Define the coordinates of the corners of the table in the output image
                 output_pts = np.array(
                     [
@@ -282,6 +338,17 @@ class PerspectiveCorrector:
                 )
 
     def correct_frame(self, frame):
+        """
+        Correct the perspective of the input frame using a provided calibration
+        matrix
+
+        Args:
+            frame (numpy.ndarray): Input image frame.
+
+        Returns:
+            numpy.ndarray or None: Corrected frame if calibration matrix is
+            provided, otherwise None.
+        """
         if self.calibrated_transform is not None:
             return cv.warpPerspective(
                 frame, self.calibrated_transform, (self.width, self.height)
@@ -292,6 +359,11 @@ class PerspectiveCorrector:
 
 class PuckDetector:
     def __init__(self) -> None:
+        """
+        Initializes a PuckDetector instance.
+
+        Loads a predefined dictionary for ArUco marker detection and configures parameters.
+        """
         # Load a predefined dictionary for ArUco marker detection
         self.dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
         # Create an instance of DetectorParameters for configuring ArUco detection
@@ -302,6 +374,19 @@ class PuckDetector:
         self.detector = cv.aruco.ArucoDetector(self.dictionary, self.parameters)
 
     def detect_puck(self, frame, starttime):
+        """
+        Detects a puck based on the presence of an ID 4 ArUco marker in the
+        provided frame
+
+        Args:
+            frame (numpy.ndarray): Input image frame.
+            starttime: a float start time for performance measurement.
+
+        Returns:
+        tuple: A tuple containing the processed frame and a TimestampedPos 
+        object representing the detected puck's position. Returns (frame, None)
+        if no puck is detected.
+        """
         if PERF_COUNTER_ENABLED:
             print(f"detect puck started at {time.perf_counter() - starttime}")
         markerCorners, markerIds, _ = self.detector.detectMarkers(frame)
@@ -330,6 +415,27 @@ class PuckDetector:
 
 
 def main():
+    """
+    Run the main program for puck detection and trajectory prediction.
+
+    This function initializes the webcam stream, perspective corrector, puck 
+    detector, and other necessary components. It continuously captures frames,
+    applies perspective transformation, detects the puck, predicts its 
+    trajectory, and displays the result with visualizations. If ARDUINO_ENABLED
+    is True, it also sends puck coordinates to an Arduino.
+
+    The program exits when the 'Esc' key is pressed.
+
+    Key Controls:
+    - 'Esc': Exit the program.
+    - 'c': Calibrate the perspective corrector using the current frame.
+
+    Note:
+        Make sure to remove the striker before running the program to avoid
+        triggering motor calibration while the two arms are physically connected
+    Returns:
+        None
+    """
     # Initialize the video capture object to capture video from the default camera (camera 0)
     stream = WebcamVideoStream(src=4).start()
     fps = FPS().start()
